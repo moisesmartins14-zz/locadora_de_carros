5df3127e5b1aea117fdc42eb657b0b8100b8d419
Moises Martins <moisesmartins2584@gmail.com>
2020-10-05 21:49:45 -0400

    commit #1
    projeto gerado

---
+++ src/main/java/com/martins/demo/LocacaoDeCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.martins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocacaoDeCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocacaoDeCarrosApplication.class, args);
+	}
+
+}
